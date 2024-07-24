9b9b2f8f930b9386cd61c0b40dc9cce3ebab3373
ZuzannaLeliwa <zleliwa1@sheffield.ac.uk>
2024-07-24 16:27:49 +0100

    Update add_spec.py

--- add_spec.py
+++ add_spec.py
@@ -2,7 +2,36 @@ import ROOT
 
 import numpy as np
 
-def add_spec(spectra):
+def extend_spec(NewMax, NewBins, og_spec):
+    extended = ROOT.TH1D("extended" +str(NewMax), "", NewBins, 0, NewMax)
+
+    for i in range(og_spec.GetNbinsX()):
+        bin_center = og_spec.GetBinCenter(i)
+        content = og_spec.GetBinContent(i)
+        error = og_spec.GetBinError(i)
+        new_bin = og_spec.FindBin(bin_center)
+        extended.SetBinContent(new_bin, content)
+        extended.SetBinError(new_bin, error)
+
+    return extended
+
+def add_byStack(spectra):
+    maxima = []
+    for i in range(len(spectra)):
+        maxima.append(spectra[i].GetXaxis().GetXmax())
+        
+
+    for i in range(len(spectra)):
+        spectra[i].GetXaxis().SetLimits(0, max(maxima))
+
+    hsum = ROOT.THStack("spectra","")
+
+    for i in range(len(spectra)):
+        hsum.Add(spectra[i])
+
+    return hsum
+
+def add_byMerge(spectra):
 
     maxima = []
     for i in range(len(spectra)):
@@ -23,4 +52,5 @@ def add_spec(spectra):
     return hsum
 
 
+    
     
