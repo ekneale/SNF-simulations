955872b3546f418754f40eab3d41b73811dbded0
ZuzannaLeliwa <zleliwa1@sheffield.ac.uk>
2024-08-01 12:57:14 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = "snf_simulations",
+      version = "0.1",
+      description = "simulates antineutrino spectra for different compositions of spent nuclear fuel",
+      packages = ["simulation"],
+      install_requires = ['numpy'],
+      author='Zuzanna Leliwa',
+      author_email='zleliwa1@sheffield.ac.uk', 
+      zip_safe=False
+)
+   
