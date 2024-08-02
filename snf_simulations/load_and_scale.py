05e081f3c356a112970d0ef1acda9a4cd71fb3a6
ZuzannaLeliwa <zleliwa1@sheffield.ac.uk>
2024-08-02 11:49:17 +0100

    Update load_and_scale.py

--- snf_simulations/load_and_scale.py
+++ snf_simulations/load_and_scale.py
@@ -1,10 +1,12 @@
 import numpy as np
 import ROOT
 
-import scale
-import load_spec
+from snf_simulations import scale, load_spec
+
+#combining load and scale functions
 
 def load_equal_scaled(data,max_E,named, isotope, m,mr,half_life_yrs,removal_time, min_E=0):
     spec = load_spec.load_equal(named,isotope,data[:,0],data[:,1],data[:,2],max_E, min_E)
     spec_scaled = scale.scale(spec, m, mr, half_life_yrs, removal_time)
+    spec_scaled.SetTitle(isotope)
     return spec_scaled
