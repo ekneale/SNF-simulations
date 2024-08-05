23e447e4897041078454ffe47c1889cbc4997567
ZuzannaLeliwa <zleliwa1@sheffield.ac.uk>
2024-08-05 11:19:28 +0100

    Update scale.py

--- snf_simulations/scale.py
+++ snf_simulations/scale.py
@@ -1,16 +1,21 @@
-import numpy as np
-from snf_simulations import sample
 import ROOT
+import numpy as np
 
+def scale(spectrum, m, mr, half_life_yrs, removal_time):
+    #mass is inputted in kg
+    #removal time is in years
 
+    #calculation of activity of isotope
+    N0 = (m* 1000 / mr) * 6.022e23
+    A0 = N0 * (np.log(2) / (half_life_yrs *365 * 24 * 60**2))
+    A = A0 * np.exp(-1 * (np.log(2) / half_life_yrs) * removal_time)
+	
+    spectrum.SetStats(0)
 
-def flux_calc(total_spec,distance_m,N=1000000):
-    events = np.array(sample.sample(total_spec, N))
+    spectrum.Scale(A) 
 
-    total_events = total_spec.Integral() #total flux per second
+    spectrum.SetTitle("Scaled Spectrum")
+    spectrum.GetXaxis().SetTitle("Energy [keV]")
+    spectrum. GetYaxis().SetTitle("Relative Flux [keV^{-1}s^{-1}]")
 
-    flux = (1/(4*np.pi*(distance_m*100)**2)) * (total_events / len(events))
-
-    print(flux, "cm^-2 s^-1")
-
-    return flux
+    return spectrum
