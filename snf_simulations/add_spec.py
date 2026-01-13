da8563a674ca6d1a26b6cae1e9f460424ef7a337
Martin Dyer <martin.dyer@sheffield.ac.uk>
2026-01-13 11:42:49 +0000

    Remove setting limits in add_spec

--- snf_simulations/add_spec.py
+++ snf_simulations/add_spec.py
@@ -6,8 +6,9 @@ def add_spec(spectra):
 
     #adding spectra and setting limit to beyond the highest antineutrino energy in the database
 
-    for i in range(len(spectra)):
-        spectra[i].GetXaxis().SetLimits(0, 6000)
+    # TODO: removed for now: it doesn't seem to work, and it would break the tests.
+    # for i in range(len(spectra)):
+    #     spectra[i].GetXaxis().SetLimits(0, 6000)
 
     hsum = spectra[14].Clone("combined")
     hsum.Reset()
