dcec7670b0a9c0fd704db7d8b846156d466516a7
Martin Dyer <martin.dyer@sheffield.ac.uk>
2026-03-03 13:52:04 +0000

    Integrate ROOT functions in test file

--- tests/test_numpy_root.py
+++ tests/test_numpy_root.py
@@ -13,16 +13,35 @@ except ImportError:
     pytest.skip("skipping ROOT tests", allow_module_level=True)
 
 from snf_simulations.data import load_antineutrino_data, load_isotope_data
-from snf_simulations.spec import (
-    Spectrum,
-    create_spec,
-    equalise_spec,
-)
+from snf_simulations.spec import Spectrum
+
+from .test_spec import _mock_data
 
 # Suppress assert warnings from ruff
 # ruff: noqa: S101  # asserts
 
 ROOT.TH1.AddDirectory(False)  # Prevent ROOT from keeping histograms in memory
+
+
+def _get_mock_spectra() -> tuple[Spectrum, ROOT.TH1D]:
+    """Create a mock Spectrum and corresponding ROOT TH1D histogram."""
+    # Use the same mock data as in test_spec.
+    energy, flux, errors = _mock_data()
+
+    # Create the Spectrum instance
+    # Note we miss off the final flux and error values, but still pass the full
+    # array of energy values.
+    spec = Spectrum(energy, flux[:-1], errors[:-1], name="mock")
+
+    # Create the ROOT histogram
+    # Note we exclude the final data row entirely.
+    root_spec = ROOT.TH1D("mock", "mock", len(energy) - 1, np.array(energy))
+    for e, f in zip(energy[:-1], flux[:-1], strict=True):
+        root_spec.Fill(e, f)
+    for i, err in enumerate(errors[:-1], start=1):
+        root_spec.SetBinError(i, err)
+
+    return spec, root_spec
 
 
 def _root_to_arrays(spec: ROOT.TH1D) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -54,30 +73,95 @@ def _compare_spectra(spec: Spectrum, root_spec: ROOT.TH1D) -> None:
     )
 
 
-def _get_mock_spectra() -> tuple[Spectrum, ROOT.TH1D]:
-    """Create a mock Spectrum and corresponding ROOT TH1D histogram."""
-    # Create mock antineutrino spectrum data
-    # Note we have 6 flux values and 7 energy values to represent the edges of 6 bins.
-    # We also deliberately include non-uniform bin widths.
-    energy = np.array([0, 0.5, 1, 2, 3, 5, 10], dtype=float)
-    flux = np.array([10, 20, 30, 40, 50, 60], dtype=float)
-    errors = np.array([1, 2, 3, 4, 5, 6], dtype=float)
+def _equalise_root_spec(
+    spec: ROOT.TH1D,
+    max_energy: int,
+    min_energy: int = 0,
+) -> ROOT.TH1D:
+    """Convert a ROOT TH1D histogram to have equal bin widths.
 
-    # Create the Spectrum instance
-    spec = Spectrum(energy, flux, errors, name="mock")
+    Each bin will be 1 keV wide, spaced from min_energy to max_energy.
 
-    # Create the ROOT histogram
-    root_spec = ROOT.TH1D("mock", "mock", len(energy) - 1, np.array(energy))
-    for e, f in zip(energy[:-1], flux, strict=True):
-        root_spec.Fill(e, f)
-    for i, err in enumerate(errors, start=1):
-        root_spec.SetBinError(i, err)
+    Args:
+        spec: Original histogram with variable bin widths.
+        max_energy: Maximum energy for the new histogram.
+        min_energy: Minimum energy for the new histogram.
 
-    return spec, root_spec
+    Returns:
+        New histogram with equal bin widths.
+
+    """
+    # Create new bin edges and new bin centres.
+    new_edges = np.linspace(min_energy, max_energy, (max_energy - min_energy) + 1)
+    new_centres = new_edges[:-1] + 0.5
+
+    # Interpolate the content of the original histogram to the new bin centres
+    new_content = [spec.Interpolate(centre) for centre in new_centres]
+
+    # Calculate the new errors for interpolated bins
+    new_errors = []
+    for centre in new_centres:
+        # The TH1D::Interpolate function looks for the two closest bins surrounding the
+        # given point.
+        # If it's below the first bin centre or above the final bin centre it just
+        # returns the content of the first/last bin, so here we do the same.
+        if centre < spec.GetBinCenter(1):
+            new_errors.append(spec.GetBinError(1))
+            continue
+        if centre >= spec.GetBinCenter(spec.GetNbinsX()):
+            new_errors.append(spec.GetBinError(spec.GetNbinsX()))
+            continue
+
+        # Find which of the old bins the new centre would fall within.
+        idx = spec.FindBin(centre)
+
+        # Find which of the surrounding bins is lower and which is upper.
+        # We need to check which side of the centre of the found bin the new centre is.
+        if centre < spec.GetBinCenter(idx):
+            lower_centre = spec.GetBinCenter(idx - 1)
+            upper_centre = spec.GetBinCenter(idx)
+            lower_error = spec.GetBinError(idx - 1)
+            upper_error = spec.GetBinError(idx)
+        else:
+            lower_centre = spec.GetBinCenter(idx)
+            upper_centre = spec.GetBinCenter(idx + 1)
+            lower_error = spec.GetBinError(idx)
+            upper_error = spec.GetBinError(idx + 1)
+
+        # Linear interpolation of the new error.
+        # We find the new error by scaling the errors of the surrounding two bins
+        # by the distance of the new centre to each of them.
+        dist = abs(upper_centre - lower_centre)
+        dist_lower = abs(centre - lower_centre)
+        dist_upper = abs(upper_centre - centre)
+        weight_lower = (dist - dist_lower) / dist
+        weight_upper = (dist - dist_upper) / dist
+        error = np.sqrt(
+            (weight_lower * lower_error) ** 2 + (weight_upper * upper_error) ** 2,
+        )
+        new_errors.append(error)
+
+    # Create the new histogram
+    name = spec.GetName()
+    title = spec.GetTitle()
+    spec_equal = ROOT.TH1D(
+        str(name),
+        str(title) + " equal bin widths",
+        len(new_centres),
+        new_edges,
+    )
+
+    # Fill the new histogram with the interpolated content
+    for centre, content in zip(new_centres, new_content, strict=True):
+        spec_equal.Fill(centre, content)
+    for i, err in enumerate(new_errors, start=1):
+        spec_equal.SetBinError(i, err)
+
+    return spec_equal
 
 
 def test_create() -> None:
-    """Test that Spectrum constructor matches create_spec."""
+    """Test that creating the Spectrum matches ROOT."""
     spec, root_spec = _get_mock_spectra()
 
     # Compare the arrays to ensure they match
@@ -85,19 +169,19 @@ def test_create() -> None:
 
 
 def test_equalise() -> None:
-    """Test that Spectrum.equalise matches equalise_spec."""
+    """Test that Spectrum.equalise matches ROOT."""
     spec, root_spec = _get_mock_spectra()
 
     # Equalise the spectra
     spec.equalise(width=1, min_energy=0, max_energy=3)
-    root_equal = equalise_spec(root_spec, max_energy=3, min_energy=0)
+    root_equal = _equalise_root_spec(root_spec, max_energy=3, min_energy=0)
 
     # Compare the arrays to ensure they match
     _compare_spectra(spec, root_equal)
 
 
 def test_scale() -> None:
-    """Test that Spectrum scaling matches scale_spec."""
+    """Test that Spectrum scaling matches ROOT."""
     spec, root_spec = _get_mock_spectra()
 
     # Scale the spectra
@@ -240,7 +324,7 @@ def test_integrate() -> None:
 
 
 def test_spec_real() -> None:
-    """Test that Spectra from real data matches the ROOT pipeline."""
+    """Test that Spectra from real data matches ROOT."""
     max_energy = 6000
     molar_masses, _ = load_isotope_data()
     isotopes = list(molar_masses.keys())
@@ -251,8 +335,12 @@ def test_spec_real() -> None:
         errors = isotope_data[:, 2]
 
         # Create the ROOT spectrum and equalise it
-        root_spec = create_spec(energy, flux, errors, name=isotope)
-        root_equal = equalise_spec(root_spec, max_energy=max_energy, min_energy=0)
+        root_spec = ROOT.TH1D(isotope, isotope, len(energy) - 1, np.array(energy))
+        for e, f in zip(energy[:-1], flux[:-1], strict=True):
+            root_spec.Fill(e, f)
+        for i, err in enumerate(errors[:-1], start=1):
+            root_spec.SetBinError(i, err)
+        root_equal = _equalise_root_spec(root_spec, max_energy=max_energy, min_energy=0)
 
         # Create the Spectrum instance and equalise it
         spec = Spectrum.from_isotope(isotope)
